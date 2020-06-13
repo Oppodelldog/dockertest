8a4632a91e80043ee6f21d057924cc890eb53f22
Oppodelldog <odog@netcologne.de>
2020-06-13 11:51:56 +0200

    fix doc

--- doc.go
+++ doc.go
@@ -2,7 +2,7 @@
 //
 // This might be simple as executing unit tests in isolation on a docker host.
 // You might also want to execute more complex setups like functionally testing a microservice with it's dependencies.
-// This is where docker supports you.
+// This is where dockertest supports you.
 //
 // Basically this package consists of Builders to create a Network or Containers.
 // But it also helps in orchestrating the tests and finally in cleaning up.
